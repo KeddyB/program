beda181998208a03655545fabded8497136cc732
KeddyB <bokelusi@gmail.com>
2022-12-14 16:30:47 +0100

    dya 3 of 30

--- snake game 2/game.js
+++ snake game 2/game.js
@@ -9,54 +9,76 @@ var context;
 var snakeX = blockSize * 12;
 var snakeY = blockSize * 12;
 
+//snake body
+var snakeBody = [];
+
 //snake speed
-var velocityX = 0
-var velocityY = 0
+var velocityX = 0;
+var velocityY = 0;
 
 //food 
 var foodX;
 var foodY;
 
+//game over
+var gameOver = false;
+
 window.onload = function(){
     board = document.getElementById("board");
     board.height = rows * blockSize;
-    board.width = cols * blockSize
+    board.width = cols * blockSize;
     context = board.getContext("2d")
 
     foodPos();
-    document.addEventListener("keyup", changePos())
-    update();
-    setInterval(update, 100)
+    document.addEventListener("keyup", changeDir);
+    //update();
+    setInterval(update, 1000/10);
 }
 function update(){
+    if(gameOver){
+        return;
+    }
     context.fillStyle ="black";
     context.fillRect(0, 0, board.width, board.height)
 
-    context.fillStyle ="lime";
-    snakeX += velocityX
-    snakeX += velocityY
-    context.fillRect(snakeX, snakeY, blockSize, blockSize);
-
     context.fillStyle ="red";
     context.fillRect(foodX, foodY, blockSize, blockSize);
+
+    if (foodX == snakeX && foodY == snakeY){
+        snakeBody.push([foodX, foodY])
+        foodPos();
+    }
+    for (let i = snakeBody.length - 1; i > 0; i ++){
+        snakeBody[i] = snakeBody[i - 1];
+    }
+    if(snakeBody.length){
+        snakeBody[0]= [snakeX, snakeY]
+    }
+    context.fillStyle ="lime";
+    snakeX +=velocityX * blockSize;
+    snakeY +=velocityY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+    for(let i = 0; i < snakeBody.length; i++){
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }
 }
-function changePos(e){
-    if (e.code =="ArrowUp"){
+function changeDir(e){
+    if (e.code =="ArrowUp" && velocityY != 1){
         velocityX = 0;
         velocityY = -1;
-    }else if(e.code == "ArrowDown"){
+    }else if(e.code == "ArrowDown" && velocityY != -1){
         velocityX = 0;
         velocityY = 1;
-    }else if(e.code == "ArrowRight"){
-        velocityX = -1;
-        velocityY = 0;
-    }else if(e.code == "ArrowLeft"){
+    }else if(e.code == "ArrowRight" && velocityX != -1){
         velocityX = 1;
+        velocityY = 0;
+    }else if(e.code == "ArrowLeft" && velocityX != 1){
+        velocityX = -1;
         velocityY = 0;
     }
 }
 function foodPos(){
     foodX = Math.floor(Math.random() * cols) * blockSize
     foodY = Math.floor(Math.random() * rows) * blockSize
- 
 }

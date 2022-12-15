03d9fa5b34af5df60f04944d1c4750538c02f664
KeddyB <bokelusi@gmail.com>
2022-12-15 23:34:07 +0100

    still on day 4

---
+++ sprite animation/game.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d')
+const CANVAS_WIDTH = canvas.width = 600
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src =''
+
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.fillRect( 45, 45, 100, 100);
+    
+    requestAnimationFrame(animate)
+}
+animate()

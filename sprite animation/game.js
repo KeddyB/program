fd1daace7b92d494c271c70bab7f9187c3593122
KeddyB <bokelusi@gmail.com>
2022-12-16 02:41:10 +0100

    day 5 0f 30 sprite animation

--- sprite animation/game.js
+++ sprite animation/game.js
@@ -4,15 +4,20 @@ const CANVAS_WIDTH = canvas.width = 600
 const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
-playerImage.src =''
+playerImage.src = 'sprite animations/shadow_dog.png'
+const spriteWidth = 575;
+const spriteHeight = 525;
 
+let frameX = 0;
+let frameY = 0;
 
 function animate(){
-    
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillStyle = "red"
-    ctx.fillRect( x, 45, 100, 100);
-    
+    //ctx.fillStyle = "red"
+    //ctx.fillRect( 45, 45, 100, 100);
+    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+    if(frameX < 6)frameX++;
+    else frameX = 0
     requestAnimationFrame(animate)
 }
 animate()

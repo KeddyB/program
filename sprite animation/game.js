605a8669315120c97b25129c957135a521a8758c
KeddyB <bokelusi@gmail.com>
2022-12-16 01:46:06 +0100

    day 5 of 30

--- sprite animation/game.js
+++ sprite animation/game.js
@@ -6,9 +6,12 @@ const CANVAS_HEIGHT = canvas.height = 600;
 const playerImage = new Image();
 playerImage.src =''
 
+
 function animate(){
+    
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillRect( 45, 45, 100, 100);
+    ctx.fillStyle = "red"
+    ctx.fillRect( x, 45, 100, 100);
     
     requestAnimationFrame(animate)
 }

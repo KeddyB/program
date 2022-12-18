4f0cf50e5aecb9d2bb12bb6aeb0064a6efb87a7a
KeddyB <bokelusi@gmail.com>
2022-12-18 01:30:34 +0100

    day 7 of 30, losing my sanity but still pushing

--- sprite animation/game.js
+++ sprite animation/game.js
@@ -8,23 +8,78 @@ playerImage.src = 'sprite animations/shadow_dog.png'
 const spriteWidth = 575;
 const spriteHeight = 525;
 
+playerState ='sit'
+
 //this frame help to pick the pictures of the frames to display ensuring an animation
-let frameX = 0;
-let frameY = 0;
+//let frameX = 0;
+//let frameY = 0;
 
 //to slow the frames we create grame frame variable
 let gameFrame = 0;
 const staggerFrame = 5;
 
+const spriteAnimation =[];
+//create a container to hold all data and location of each corner of the picture to animate through it
+const animationPos = [
+    {
+        name: 'idle',
+        frames: 7
+    },
+    {
+        name: 'jump',
+        frames: 7
+    },
+    {
+        name: 'fall',
+        frames: 7
+    },
+    {
+        name: 'run',
+        frames: 7
+    },
+    {
+        name: 'dizzy',
+        frames: 11
+    },
+    {
+        name: 'sit',
+        frames: 5
+    },
+    {
+        name: 'roll',
+        frames: 7
+    }, 
+    {
+        name: 'bite',
+        frames: 7
+    }, 
+    {
+        name: 'ko',
+        frames: 12
+    },
+    {
+        name: 'getHit',
+        frames: 4
+    },
+];
+animationPos.forEach((pos, index) =>{
+    let frames = {
+        loc: [],
+    }
+    for (let j = 0; j < pos.frames; j++){
+        let PositionX = j * spriteWidth;
+        let PositionY = index * spriteHeight;
+        frames.loc.push({x: PositionX, y : PositionY})
+    }
+    spriteAnimation[pos.name] = frames;
+})
+console.log(spriteAnimation)
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let Position = Math.floor(gameFrame/staggerFrame) % 6;
-    frameX = spriteWidth * Position
-    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
-    //if (gameFrame % staggerFrame == 0){
-    //    if(frameX < 9)frameX++;
-    //    else frameX = 0
-    //}
+    let position = Math.floor(gameFrame/staggerFrame) % spriteAnimation[playerState].loc.length;
+    let frameX = spriteWidth * position
+    let frameY = spriteAnimation[playerState].loc[position].y;
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
     gameFrame ++;
     requestAnimationFrame(animate)
 }

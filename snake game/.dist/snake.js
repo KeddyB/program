690b1131a9c45e077aba4eb8bd6d816b05ed227f
KeddyB <bokelusi@gmail.com>
2023-01-07 15:17:08 +0000

    not animating for some reason

--- snake game/.dist/snake.js
+++
@@ -1,9 +0,0 @@
-export const SNAKE_SPEED = 1;
-const snakeBody = [{x:11, y:11}]
-export function update(){
-    console.log('update snake')
-}
-
-export function draw(){
-    console.log('draw snake')
-}
--- vision and audio feedback/app.js
+++ vision and audio feedback/app.js
@@ -5,8 +5,8 @@ canvas.height = 500;
 const explosions = []
 let canvasPosition = canvas.getBoundingClientRect();
 
-class Explosion {
-    constructor(x,y){
+class Explosion{
+    constructor(x, y){
         this.x = x;
         this.y = y;
         this.spriteWidth = 200;
@@ -14,22 +14,26 @@ class Explosion {
         this.width = this.spriteWidth * 0.5;
         this.height = this.spriteHeight * 0.5;
         this.image = new Image()
-        this.image = 'sprite animations/boom.png'
+        this.image.src = 'boom.png';
         this.frame = 0;
+        this.timer = 0;
     }
     update(){
-        this.frame++;
+        this.timer++
+        if(this.timer % 10 === 0){
+            this.frame++;
+        }
     }
     draw(){
         ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y,this.width, this.heigth)
     }
 }
+
 window.addEventListener('click', function(e){
     let positionX = e.x - canvasPosition.left;
     let positionY = e.y - canvasPosition.top;
-    explosions.push(new Explosion(positionX, positionY))
+    explosions.push(new Explosion(positionX, positionY));
 });
-
 
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)

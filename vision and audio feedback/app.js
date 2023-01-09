e855e6056634471f501982e223fd7b0c1b8b8c4e
KeddyB <bokelusi@gmail.com>
2023-01-09 10:03:56 +0100

    day 30

--- vision and audio feedback/app.js
+++ vision and audio feedback/app.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d')
-canvas.width = 320;
+canvas.width = 360;
 canvas.height = 500;
 const explosions = []
 let canvasPosition = canvas.getBoundingClientRect();

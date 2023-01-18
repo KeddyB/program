32165075c697f16811e2fa6d5e6c06ffb6081f21
KeddyB <bokelusi@gmail.com>
2023-01-18 22:27:59 +0100

    comeback tomorrow

--- vision and audio feedback/app.js
+++ vision and audio feedback/app.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d')
 canvas.width = 360;
 canvas.height = 500;
-const explosions = []
+var explosions = []
 let canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion{
@@ -22,7 +22,7 @@ class Explosion{
         this.frame++
     }
     draw(){
-        ctx.drawImage(this.img, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y,this.width, this.heigth)
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.heigth)
     }
 }
 
@@ -30,16 +30,12 @@ window.addEventListener('click', function(e){
     console.log(e)
     let positionX = e.x - canvasPosition.left;
     let positionY = e.y - canvasPosition.top;
-    explosions.push(new Explosion(3, 2));
-    
+    explosions.push(new Explosion(positionX, positionY))
 });
-
 function animate(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    //for(let i = 0; i < explosions.length; i++){
-       // explosions[i].update();
-        //explosions[i].draw();
-    //}
+    for(let i = 0; i > explosions.length; i++){
+        explosions[i].update();
+        explosions[i].draw();
+    }
     requestAnimationFrame(animate)
 }
-animate()

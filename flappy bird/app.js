8a798b64310ddd1aa110551b06591fb09eeae568
KeddyB <bokelusi@gmail.com>
2023-03-02 22:24:58 +0100

    drawing onload bird

--- flappy bird/app.js
+++ flappy bird/app.js
@@ -1,14 +1,32 @@
-let img = new Image ()
-img.src = "./flappybird.png"
+//canvas
+let canvas;
+let ctx;
+let canvasWidth = 360;
+let canvasHeight = 640;
+
+//bird
+birdWidth = 34
+birdHeight = 24
+let birdX = canvasWidth/8
+let birdY = canvasHeight/2
+
+let bird = {
+    x: birdX,
+    y: birdY,
+    width: birdWidth,
+    height: birdHeight,
+}
+
 window.onload = function(){
     canvas = document.getElementById("canvas1");
     ctx = canvas.getContext("2d");
-    
-    canvas.width = 360;
-    canvas.height = 640;
-    animate()
-}
-function animate(){
-    ctx.fillStyle = "green"
-    ctx.drawImage(img, 50, 320, 40, 28)
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+
+    //draw bird
+    birdImg = new Image()
+    birdImg.src = "./flappybird.png"
+    birdImg.onload = function(){
+        ctx.drawImage(birdImg, bird.x, bird.y,bird.width, bird.height)
+    } 
 }

e8cad8cc215b48d329c57e98a3ef107e71dfc88e
KeddyB <bokelusi@gmail.com>
2023-03-02 23:28:19 +0100

    flappybird game

--- flappy bird/app.js
+++ flappy bird/app.js
@@ -16,6 +16,20 @@ let bird = {
     width: birdWidth,
     height: birdHeight,
 }
+//pipes
+let pipeArray = [];
+let pipeWidth = 64
+let pipeHeight = 512
+let pipeX = canvasWidth;
+let pipeY = 0
+
+let topPipeImg;
+let bottomPipeImg
+
+//physics
+let velocityX = -2
+let velocityY = 0
+let gravity = .5
 
 window.onload = function(){
     canvas = document.getElementById("canvas1");
@@ -29,4 +43,58 @@ window.onload = function(){
     birdImg.onload = function(){
         ctx.drawImage(birdImg, bird.x, bird.y,bird.width, bird.height)
     } 
+    //drawpipes
+    topPipeImg = new Image();
+    topPipeImg.src = "./toppipe.png"
+    bottomPipeImg = new Image()
+    bottomPipeImg.src = "./bottompipe.png"
+
+    requestAnimationFrame(update)
+    setInterval(placePipes, 1500)
+
+    document.addEventListener("keydown", (e)=>{
+        if(e.code == "Space" || e.code == "ArrowUp"){
+            velocityY = -6
+        }
+    })
+}
+function update(){
+    requestAnimationFrame(update)
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+
+    velocityY += gravity
+
+    bird.y = Math.max(bird.y + velocityY, 0)
+    ctx.drawImage(birdImg, bird.x, bird.y,bird.width, bird.height)
+
+    //pipes
+    for(let i = 0; i < pipeArray.length; i++){
+        let pipe = pipeArray[i]
+        pipe.x += velocityX;
+        ctx.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height)
+    }
+}
+
+function placePipes(){
+    let randomPipeY = pipeY - pipeHeight/4 - Math.floor(Math.random() * (pipeHeight/2))
+    let opening = canvasHeight/4
+    let topPipe = {
+        img: topPipeImg,
+        x: pipeX,
+        y: randomPipeY,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
+    }
+    pipeArray.push(topPipe)
+
+    let bottomPipe = {
+        img: bottomPipeImg,
+        x: pipeX,
+        y: randomPipeY + opening + pipeHeight,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
+    }
+    pipeArray.push(bottomPipe)
 }

4305062db58aec92098ad378296c45f51a8f34b4
KeddyB <bokelusi@gmail.com>
2023-03-16 14:24:41 +0100

    detecting for colission

--- self driving car neural network/sensors.js
+++ self driving car neural network/sensors.js
@@ -1,20 +1,36 @@
 class Sensor{
     constructor(car){
         this.car = car;
-        this.rayCount = 3;
+        this.rayCount = 5;
         this.rayLength = 100;
-        this.raySpread = Math.PI/4
+        this.raySpread = Math.PI/2
 
         this.rays = []
+
+        this.readings = []
     }
-    update(){
+    update(roadBorders){
+        this.#castRays();
+        this.readings = [];
+        for(let i = 0; i<this.rays.length; i++){
+            this.readings.push(
+                this.#getReading(this.rays[i], roadBorders)
+            )
+        }
+    }
+
+    #getReading(){
+        
+    }
+
+    #castRays(){
         this.rays = []
         for(let i =0; i<this.rayCount; i++){
             const rayAngle = lerp(
                 this.raySpread/2,
                 -this.raySpread/2,
-                i/(this.rayCount-1)
-            );
+                this.rayCount==1?0.5:(i/(this.rayCount-1))
+            ) + this.car.angle;
 
             const start = {x: this.car.x, y:this.car.y}
             const end = {
@@ -26,14 +42,14 @@ class Sensor{
     }
     draw(ctx){
         for(let i = 0; i<this.rayCount; i++){
-            ctx.beginPath
+            ctx.beginPath()
             ctx.lineWidth = 2
             ctx.strokeStyle = "yellow";
             ctx.moveTo(
                 this.rays[i][0].x,
                 this.rays[i][0].y
             );
-            ctx.moveTo(
+            ctx.lineTo(
                 this.rays[i][1].x,
                 this.rays[i][1].y
             );

19b9da61be9e9118f23095a96e2f3f254e98a16e
KeddyB <bokelusi@gmail.com>
2023-03-16 12:48:15 +0100

    road and utility

--- self driving car neural network/road.js
+++ self driving car neural network/road.js
@@ -15,14 +15,16 @@ class Road{
         ctx.lineWidth = 5;
         ctx.strokeStyle = 'white';
 
-        ctx.beginPath();
-        ctx.moveTo(this.left, this.top);
-        ctx.lineTo(this.left, this.bottom);
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(this.right, this.top);
-        ctx.lineTo(this.right, this.bottom);
-        ctx.stroke();
+        for(let i=0; i<=this.laneCount; i++){
+            const x = lerp(
+                this.left,
+                this.right,
+                i/this.laneCount
+            );
+            ctx.beginPath();
+            ctx.moveTo(x, this.top);
+            ctx.lineTo(x, this.bottom);
+            ctx.stroke();
+        }
     }
 }

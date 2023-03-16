205d67815bf275463bd8c997a447682bcddefaaf
KeddyB <bokelusi@gmail.com>
2023-03-16 12:01:49 +0100

    currently making roads

--- self driving car neural network/app.js
+++ self driving car neural network/app.js
@@ -2,15 +2,16 @@ canvas = document.getElementById("canvas");
 canvas.width = 200
 
 const ctx = canvas.getContext("2d")
-
+const road = new Road(canvas.width/2, canvas.width * .9)
 const car = new Car(100, 100, 30, 50)
-car.draw(ctx)
 
 animate()
 
 function animate(){
     car.update()
+
     canvas.height = window.innerHeight
+    road.draw(ctx)
     car.draw(ctx)
     requestAnimationFrame(animate)
 }
--- self driving car neural network/road.js
+++ self driving car neural network/road.js
@@ -2,13 +2,14 @@ class Road{
     constructor( x, width, laneCount = 3){
         this.x = x
         this.width = width
+        this.laneCount = laneCount
 
         this.left = x-width/2;
         this.right = x+width/2;
 
-        const infinity = 10000000;
+        const infinity = 1000000;
         this.top = -infinity;
-        this.bottom = -infinity;
+        this.bottom = infinity;
     }
     draw(ctx){
         ctx.lineWidth = 5;

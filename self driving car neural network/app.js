767d862a14447dbc4b15238cfea3cdf4cfc062f9
KeddyB <bokelusi@gmail.com>
2023-03-26 12:05:40 +0100

    commit

--- self driving car neural network/app.js
+++ self driving car neural network/app.js
@@ -8,7 +8,8 @@ const carCtx = carCanvas.getContext("2d")
 const networkCtx = networkCanvas.getContext("2d")
 const road = new Road(carCanvas.width/2, carCanvas.width * .9)
 
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI", 3)
+const N = 100
+const cars = generateCars(N)
 
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)
@@ -18,8 +19,8 @@ animate()
 
 function generateCars(N){
     const cars = []
-    for(let i = 0; i <= N; i++){
-        cars.push(new Car(road.getLaneCenter(1),100,30,50,"AI"))
+    for(let i = 1; i <= N; i++){
+        cars[i].push(new Car(road.getLaneCenter(1),100,30,50,"AI"))
     }
     return
 }
@@ -28,25 +29,27 @@ function animate(time){
     for(let i=0; i < traffic.length; i++){
         traffic[i].update(road.borders, [])
     }
-    
-        car.update(road.borders, traffic)
-
+    for(let i=0; i < cars.length; i++){
+        cars[i].update(road.borders, traffic)
+    }
     carCanvas.height = window.innerHeight
     networkCanvas.height = window.innerHeight
 
     carCtx.save()
-    carCtx.translate(0, -car.y + carCanvas.height * 0.7)
+    carCtx.translate(0, -cars[0].y + carCanvas.height * 0.7)
     road.draw(carCtx)
 
     for(let i=0; i<traffic.length; i++){
         traffic[i].draw(carCtx, "red")
     }
-        car.draw(carCtx, "blue")
-    
+        
+    for(let i=0; i < cars.length; i++){
+        cars[i].draw(carCtx, "blue")
+    }
 
     carCtx.restore()
 
     networkCtx.lineDashOffset = -time/50
-    Visualizer.drawNetwork(networkCtx,car.brain)
+    Visualizer.drawNetwork(networkCtx, cars[0].brain)
     requestAnimationFrame(animate)
 }

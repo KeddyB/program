a3c9d95590861dd9a9f247ceb274e18f9af8b370
KeddyB <bokelusi@gmail.com>
2023-03-26 14:42:37 +0100

    done for now

--- self driving car neural network/car.js
+++ self driving car neural network/car.js
@@ -21,6 +21,8 @@ class Car {
             )
         }
         this.controls = new Controls(controlType);
+        this.img = new Image
+        this.img.src = "car.png"
     }
 
     update(roadBorders, traffic){
@@ -120,19 +122,31 @@ class Car {
         this.y -= Math.cos(this.angle) * this.speed
     }
     draw(ctx, color, drawSensor= false){
-        if(this.damaged ){
-            ctx.fillStyle = "gray"
-        }
-        else{
-            ctx.fillStyle = color
-        }
+        // if(this.damaged ){
+        //     ctx.fillStyle = "gray"
+        // }
+        // else{
+        //     ctx.fillStyle = color
+        // }
+        // ctx.save()
+        // ctx.beginPath()
+        // ctx.moveTo(this.polygon[0].x, this.polygon[0].y)
+        // for(let i = 1; i<this.polygon.length; i++){
+        //     ctx.lineTo(this.polygon[i].x, this.polygon[i].y)
+        // }
+        // ctx.fill();
+
         ctx.save()
-        ctx.beginPath()
-        ctx.moveTo(this.polygon[0].x, this.polygon[0].y)
-        for(let i = 1; i<this.polygon.length; i++){
-            ctx.lineTo(this.polygon[i].x, this.polygon[i].y)
-        }
-        ctx.fill();
+        ctx.translate(this.x, this.y)
+        ctx.rotate(-this.angle)
+        ctx.drawImage(
+            this.img,
+            -this.width/2,
+            -this.height/2,
+            this.width,
+            this.height
+        )
+        ctx.restore()
 
         if(this.sensor && drawSensor){
             this.sensor.draw(ctx)

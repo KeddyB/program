00a8fb3d44b38cae6ba49d24ea8dd32fc7fcb902
KeddyB <bokelusi@gmail.com>
2023-03-26 01:41:28 +0100

    visualizer

--- self driving car neural network/app.js
+++ self driving car neural network/app.js
@@ -5,7 +5,7 @@ networkCanvas = document.getElementById("networkCanvas");
 networkCanvas.width = 300
 
 const carCtx = carCanvas.getContext("2d")
-const networkCtx = carCanvas.getContext("2d")
+const networkCtx = networkCanvas.getContext("2d")
 const road = new Road(carCanvas.width/2, carCanvas.width * .9)
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI")
 

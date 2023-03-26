e17fd7e31f54fc65363cd131ea086b1ec5c3dfc6
KeddyB <bokelusi@gmail.com>
2023-03-26 12:49:03 +0100

    commit

--- self driving car neural network/app.js
+++ self driving car neural network/app.js
@@ -18,7 +18,9 @@ if(localStorage.getItem("bestBrain")){
 }
 
 const traffic = [
-    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2)
 ]
 
 animate()

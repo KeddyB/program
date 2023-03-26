4c3d03e40cfb1b151458c35cb4077b85b6c3d49e
KeddyB <bokelusi@gmail.com>
2023-03-26 01:18:56 +0100

    networkCanvas

--- self driving car neural network/app.js
+++ self driving car neural network/app.js
@@ -1,8 +1,12 @@
-canvas = document.getElementById("canvas");
-canvas.width = 200
+carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 200
 
-const ctx = canvas.getContext("2d")
-const road = new Road(canvas.width/2, canvas.width * .9)
+networkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width = 300
+
+const carCtx = carCanvas.getContext("2d")
+const networkCtx = carCanvas.getContext("2d")
+const road = new Road(carCanvas.width/2, carCanvas.width * .9)
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI")
 
 const traffic = [
@@ -17,16 +21,17 @@ function animate(){
     }
     car.update(road.borders, traffic)
 
-    canvas.height = window.innerHeight
+    carCanvas.height = window.innerHeight
+    networkCanvas.height = window.innerHeight
 
-    ctx.save()
-    ctx.translate(0, -car.y + canvas.height * 0.7)
-    road.draw(ctx)
+    carCtx.save()
+    carCtx.translate(0, -car.y + carCanvas.height * 0.7)
+    road.draw(carCtx)
 
     for(let i=0; i<traffic.length; i++){
-        traffic[i].draw(ctx, "red")
+        traffic[i].draw(carCtx, "red")
     }
-    car.draw(ctx, "blue")
-    ctx.restore()
+    car.draw(carCtx, "blue")
+    carCtx.restore()
     requestAnimationFrame(animate)
 }

f2a6e1f8a287a74719c0ed21c42fd01dac44ad7b
KeddyB <bokelusi@gmail.com>
2023-03-29 23:01:14 +0100

    commit

--- tetris/app.js
+++ tetris/app.js
@@ -1,1 +1,15 @@
-canvas = document.getElementById("canvas1")
+let canvas = document.getElementById("canvas1")
+let ctx = canvas.getContext("2d")
+let gBArrayHeight = 20
+let gBArrayWidth = 12
+let startX = 4
+let startY = 0
+let coordinateArray = [...Array(gBArrayHeight)].map(e=> Array(gBArrayWidth).fill(0))
+let curTet = [[1,o],[0,1],[1,1],[2,1]]
+
+class Coordinates{
+    constructor(x,y){
+        this.x=x
+        this.y=y
+    }
+}

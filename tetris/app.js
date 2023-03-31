d5a4c0d42a2d393c18f81da8f347ee79afbcafcd
KeddyB <bokelusi@gmail.com>
2023-03-31 21:29:18 +0100

    commit

--- tetris/app.js
+++ tetris/app.js
@@ -1,4 +1,4 @@
-let canvas = document.getElementById("canvas1")
+let canvas
 let ctx = canvas.getContext("2d")
 let gBArrayHeight = 20
 let gBArrayWidth = 12
@@ -12,4 +12,36 @@ class Coordinates{
         this.x=x
         this.y=y
     }
+}
+document.addEventListener("DOMContentLoaded", SetupCanvas)
+
+function CreateCoordArray(){
+    let i = 0, j = 0
+    for(let y = 9; y<=446; y+=23){
+        for(let x = 11; x<=264; x+=23){
+            coordinateArray[i][j] = new Coordinates(x, y)
+            i++
+        }
+        j++
+        i=0
+    }
+}
+function SetupCanvas(){
+    canvas = document.getElementById("canvas1")
+    canvas.width = 936
+    canvas.height = 956
+
+    ctx.scale(2,2)
+    ctx.fillStyle = "white"
+    ctx.fillRect(0,0, canvas.width, canvas.height)
+
+    ctx.strokeStyle = "black"
+    ctx.strokeRect(8, 8, 280, 462)
+
+    CreateCoordArray()
+    DrawTetramino()
+}
+
+function DrawTetramino(){
+    
 }

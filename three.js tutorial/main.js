3594174195d04d1d47506b6248d10dfb6ba2790e
KeddyB <bokelusi@gmail.com>
2023-05-06 00:48:50 +0100

    commit message

--- three.js tutorial/main.js
+++ three.js tutorial/main.js
@@ -6,7 +6,23 @@ const scene = new THREE.Scene();
 //create sphere
 const geometry = new THREE.SphereGeometry(3, 64, 64)
 const material = new THREE.MeshStandardMaterial({
-   color: "#00ff83"
+   color: "#00ff83",
 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+//light
+const light = new THREE.PointLight(0xffffff, 1, 100)
+light.position.set(0, 10, 10)
+scene.add(light)
+
+//camera perspective
+const camera = new THREE.PerspectiveCamera(45, 800/600)
+camera.position.z = 20
+scene.add(camera)
+
+//rendering the scene
+const canvas = document.querySelector(".webgl")
+const renderer = new THREE.WebGLRenderer({canvas})
+renderer.setSize(800, 600)
+renderer.render(scene, camera)

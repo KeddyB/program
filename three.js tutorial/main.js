bf9503d495027b0c8c35b7594aa104867eb1e331
KeddyB <bokelusi@gmail.com>
2023-05-06 00:07:17 +0100

    learning 3d js

---
+++ three.js tutorial/main.js
@@ -0,0 +1,10 @@
+import * as THREE from 'three';
+
+//scene
+const scene = new THREE.Scene();
+
+//create sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64)
+const material = new THREE.MeshStandardMaterial({
+   color: "#00ff83"
+})

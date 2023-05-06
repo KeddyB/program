64819609500101e4c7f63ff679b69a674b17485d
KeddyB <bokelusi@gmail.com>
2023-05-06 16:29:32 +0100

    three.js

--- three.js tutorial/main.js
+++ three.js tutorial/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import './style.css'
 
 //scene
 const scene = new THREE.Scene();
@@ -11,18 +12,24 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+//sizes
+const sizes = {
+   width: window.innerWidth,
+   height: window.innerHeight
+}
+
 //light
 const light = new THREE.PointLight(0xffffff, 1, 100)
 light.position.set(0, 10, 10)
 scene.add(light)
 
 //camera perspective
-const camera = new THREE.PerspectiveCamera(45, 800/600)
+const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height, 0.1, 100)
 camera.position.z = 20
 scene.add(camera)
 
 //rendering the scene
 const canvas = document.querySelector(".webgl")
 const renderer = new THREE.WebGLRenderer({canvas})
-renderer.setSize(800, 600)
+renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)

e40949f580b6726790b88fd6355727f0aa9d0980
KeddyB <bokelusi@gmail.com>
2023-05-17 23:14:15 +0100

    commit

--- google game/app.js
+++ google game/app.js
@@ -4,6 +4,20 @@ ctx = canvas.getContext("2d")
 canvas.width = 800
 canvas.height = 200
 
+var char = {
+   x: 120,
+   y: 105
+}
+
 ctx.fillRect(0,150, canvas.width, 100)
 
-ctx.fill
+ctx.fillStyle = "red"
+ctx.fillRect(char.x,char.y, 30,50)
+
+function moveChar(){
+   document.addEventListener(onkeydown, (e)=>{
+
+      char.y += 2
+   })
+   requestAnimationFrame(moveChar)
+}

464bfd66b975b06ed66d65983227913d61d89eec
KeddyB <bokelusi@gmail.com>
2023-05-17 23:50:16 +0100

    commit

--- google game/app.js
+++ google game/app.js
@@ -1,14 +1,24 @@
 let x = 120
-let y= 105
+let y = 105
 
 window.onload = function(){
    canvas = document.querySelector(".canvas")
    ctx = canvas.getContext("2d")
-
    canvas.width = 800
    canvas.height = 200
-   ctx.fillRect(0,150, canvas.width, 100)
 
+   function update(){
+      requestAnimationFrame(update)
+      
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+      ctx.fillStyle = "black"
+      ctx.fillRect(0,150, canvas.width, 100)
+
+      ctx.fillStyle = "red"
+      ctx.fillRect(x, y, 30,50)
+      
+      moveChar()
+   }
    function moveChar(){
       document.addEventListener("keydown", (e)=>{
          if(e.key == "ArrowUp" || e.key == "Spaace"){
@@ -16,13 +26,8 @@ window.onload = function(){
             console.log(e.key)
          }
       })
+      
    }
-   function update(){
-      requestAnimationFrame(update)
-      ctx.fillStyle = "red"
-      ctx.fillRect(x, y, 30,50)
-      moveChar()
-   }
-   update()
+   setInterval(update, 100)
 }
 
